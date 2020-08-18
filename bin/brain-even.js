6d2a109efd62e5fbcf3cf4f2f34611646e9b7484
Bonauze <bonauze@gmail.com>
2020-08-18 19:52:25 +0300

    refactoring brain-even game

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,56 +1,5 @@
 #!/usr/bin/env node
 
-import readlineSync from 'readline-sync';
+import runBrainEvenGame from '../src/games/brain-even.js';
 
-const getAnswerToQuestion = (questionText) => readlineSync.question(questionText);
-
-const getRandomNumber = (min, max) => Math.round(min - 0.5 + Math.random() * (max - min + 1));
-
-const generateQuestionsAndAnswers = (quantity) => {
-  const result = [];
-
-  for (let i = 0; i < quantity; i += 1) {
-    const randomNumber = getRandomNumber(1, 100);
-    const isEven = randomNumber % 2 === 0;
-
-    result.push({
-      question: `Question: ${randomNumber}`,
-      answer: isEven ? 'yes' : 'no',
-    });
-  }
-
-  return result;
-};
-
-const startGame = (questionsAndAnswers, userName) => {
-  const isWinner = questionsAndAnswers.every((questionAndAnswer) => {
-    const { question, answer } = questionAndAnswer;
-
-    console.log(question);
-
-    const userAnswer = getAnswerToQuestion('Your answer: ');
-
-    if (userAnswer === answer) {
-      console.log('Correct!');
-      return true;
-    }
-
-    console.log(`"${userAnswer}" is wrong answer ;(. Correct answer was "${answer}".`);
-    console.log(`Let's try again, ${userName}!`);
-    return false;
-  });
-
-  if (isWinner) {
-    console.log(`Congratulations, ${userName}!`);
-  }
-};
-
-console.log('Welcome to the Brain Games!');
-
-const userName = getAnswerToQuestion('May I have your name? ');
-const questionsAndAnswers = generateQuestionsAndAnswers(3);
-
-console.log(`Hello, ${userName}!`);
-console.log('Answer "yes" if the number is even, otherwise answer "no".');
-
-startGame(questionsAndAnswers, userName);
+runBrainEvenGame();
---
+++ src/games/brain-even.js
@@ -0,0 +1,21 @@
+import getRandomNumber from '../utils/get-random-number.js';
+import GameEngine from '../game-engine.js';
+
+const TASK_DESCRIPTION = 'Answer "yes" if the number is even, otherwise answer "no".';
+
+const getGameData = () => {
+  const number = getRandomNumber(1, 100);
+  const isEven = number % 2 === 0;
+
+  return {
+    question: number,
+    answer: isEven ? 'yes' : 'no',
+  };
+};
+
+const runBrainEvenGame = () => {
+  const gameEngine = new GameEngine(TASK_DESCRIPTION, getGameData);
+  gameEngine.start();
+};
+
+export default runBrainEvenGame;
